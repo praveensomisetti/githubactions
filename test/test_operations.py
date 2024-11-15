8dc5b36e7904f0141e30d3e7c7f0f8cdc50737b9
Praveen Somisetti <spraveen4512@gmail.com>
2024-11-15 13:10:13 +0530

    Update test_operations.py

--- test/test_operations.py
+++ test/test_operations.py
@@ -4,6 +4,8 @@ def test_add():
     assert add(2,3)==5
     assert add(4,5)==9
     assert add(-2,3)==1
+    assert add(45,40)==95
+    assert add(0,-90)=90
     
 def test_sub():
     assert sub(-6,4)==-10
